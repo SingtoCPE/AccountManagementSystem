8d9cbb33bc8006b4b144661cb28bde51824334e5
singtoCPE <thitipat_cpe18@hotmail.com.com>
2020-01-21 12:41:47 +0700

    update project.

--- client/src/pages/ReportPage.vue
+++ client/src/pages/ReportPage.vue
@@ -29,8 +29,8 @@
                     height="225"
                     outlined
                   >
-                    <v-card-title>{{ item[0] }}</v-card-title>
-                    <v-card-text class="content-item">{{ item[1] }}</v-card-text>
+                    <v-card-title>{{ item.name }}</v-card-title>
+                    <v-card-text class="content-item">{{ item.text }}</v-card-text>
                     <v-card-actions class="d-flex flex-wrap align-end">
                       <v-btn
                         @click="clickViewAll(index)"
@@ -58,8 +58,8 @@
                     height="225"
                     outlined
                   >
-                    <v-card-title>{{ item[0] }}</v-card-title>
-                    <v-card-text class="content-item">{{ item[1] }}</v-card-text>
+                    <v-card-title>{{ item.name }}</v-card-title>
+                    <v-card-text class="content-item">{{ item.text }}</v-card-text>
                     <v-card-actions class="d-flex flex-wrap align-end">
                       <v-btn
                         @click="clickViewAll(index)"
@@ -74,43 +74,30 @@
         </v-tab-item>
 
         <v-tab-item>
-          <v-card
-            class="d-flex flex-wrap justify-start mt-6"
-            flat
-          >
-            <v-card
-              class="pa-2 ma-1"
-              height="215"
-              outlined
-              tile
-              width="335"
-            >
-              <v-card
-                class="pa-4 mb-2 text-start"
-                color="white"
-                height="150"
-                outlined
-              >
-                <h3>Products</h3>
-                <v-card
-                  color="white"
-                  height="80"
-                  outlined
+          <v-card elevation="0">
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  md="3"
                 >
-                  <h4>View Sales revenue by product or service.</h4>
-                </v-card>
-              </v-card>
-              <v-card
-                class="text-start ml-5"
-                color="white"
-                flat
-              >
-                <v-btn
-                  @click="crossCheckAllProduct"
-                  color="teal lighten-3"
-                >VIEW REPORT</v-btn>
-              </v-card>
-            </v-card>
+                  <v-card
+                    height="225"
+                    outlined
+                  >
+                    <v-card-title>Products</v-card-title>
+                    <v-card-text class="content-item">View Sales revenue by product or service.</v-card-text>
+
+                    <v-card-actions class="d-flex flex-wrap align-end">
+                      <v-btn
+                        @click="crossCheckAllProduct"
+                        color="teal lighten-3"
+                      >VIEW REPORT</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
           </v-card>
         </v-tab-item>
       </v-tabs>
@@ -122,37 +109,37 @@ export default {
   name: "ReportPage",
   data: () => ({
     itemInSell: [
-      [
-        "Sales",
-        "Gives you a summary and helps you keep track of your sales for the month."
-      ],
-      [
-        "Clients",
-        "Know who your best clients are so that you can focus you time and energy to serve them."
-      ],
-      ["Price Quotation", "Summarizes and help you keep track of your."],
-      [
-        "Billing Note",
-        "Summarizes and help you keep track of your billing Note/Invoice."
-      ],
-      [
-        "Payment Collection",
-        "Display the payment collection details of each invoice."
-      ],
-      [
-        "Receipt Collection",
-        "Display the payment collection details of each invoice."
-      ]
+      {
+        name: "Sales",
+        text: "Gives you a summary and helps you keep track of your sales for the month."
+      },
+      {
+        name: "Clients",
+        text: "Know who your best clients are so that you can focus you time and energy to serve them."
+      },
+      { name: "Price Quotation", text: "Summarizes and help you keep track of your." },
+      {
+        name: "Billing Note",
+        text: "Summarizes and help you keep track of your billing Note/Invoice."
+      },
+      {
+        name: "Payment Collection",
+        text: "Display the payment collection details of each invoice."
+      },
+      {
+        name: "Receipt Collection",
+        text: "Display the payment collection details of each invoice."
+      }
     ],
     itemInBuy: [
-      [
-        "Vendor",
-        "Helps you understand who your most important suppliers are and how much you are purchasing from them."
-      ],
-      [
-        "Cash Payments",
-        "Summarize cash payments from purchase order and receiving inventory documents.."
-      ]
+      {
+        name: "Vendor",
+        text: "Helps you understand who your most important suppliers are and how much you are purchasing from them."
+      },
+      {
+        name: "Cash Payments",
+        text: "Summarize cash payments from purchase order and receiving inventory documents.."
+      }
     ]
   }),
   methods: {
